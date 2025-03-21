0169a79e37afc520479872a83a8fb68ca3cfb5d4
Jose Augusto <splgugulive1@gmail.com>
2025-03-20 23:58:39 -0300

    Adicionar arquivos de projeto.

---
+++ VisualVbTeste/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox6 = New TextBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+        DataGridView1.AllowUserToResizeColumns = False
+        DataGridView1.AllowUserToResizeRows = False
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(38, 34)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(716, 150)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(649, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(105, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Visualizar Dados"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(58, 274)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "Adicionar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(167, 274)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 3
+        Button3.Text = "Editar"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(271, 274)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 4
+        Button4.Text = "Deletar"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(22, 223)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 6
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(142, 223)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 7
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(260, 223)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 8
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(388, 223)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 9
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(643, 223)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 10
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(38, 201)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(40, 15)
+        Label1.TabIndex = 11
+        Label1.Text = "Nome"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(167, 205)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(28, 15)
+        Label2.TabIndex = 12
+        Label2.Text = "CPF"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(291, 205)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(33, 15)
+        Label3.TabIndex = 13
+        Label3.Text = "Valor"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(419, 205)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(31, 15)
+        Label4.TabIndex = 14
+        Label4.Text = "Data"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(674, 205)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(30, 15)
+        Label5.TabIndex = 15
+        Label5.Text = "Tipo"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(514, 223)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 16
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(530, 205)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(58, 15)
+        Label6.TabIndex = 17
+        Label6.Text = "Descrição"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.ForeColor = Color.Red
+        Label7.Location = New Point(32, 8)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(49, 15)
+        Label7.TabIndex = 18
+        Label7.Text = "Clientes"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 320)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(TextBox6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Name = "Form1"
+        Text = "DBcred"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+
+End Class
---
+++ VisualVbTeste/Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Data.SqlClient ' Importa namespace para SQL Server
+
+Public Class Form1
+    ' Evento do botão que carregará os dados
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CarregarDados() ' Carrega dados no DataGridView
+    End Sub
+
+    ' Método para carregar dados no DataGridView
+    Private Sub CarregarDados()
+        Dim connString As String = "Data Source=localhost;Initial Catalog=Financeiro;Integrated Security=True"
+        Dim query As String = "SELECT * FROM Transacoes"
+
+        Using conn As New SqlConnection(connString)
+            Try
+                conn.Open()
+                Dim adapter As New SqlDataAdapter(query, conn)
+                Dim tabela As New DataTable()
+                adapter.Fill(tabela)
+                DataGridView1.DataSource = tabela ' Exibe a tabela no DataGridView
+            Catch ex As Exception
+                MessageBox.Show("Erro ao carregar os dados: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    ' Botão para adicionar um registro ao banco de dados
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim connString As String = "Data Source=localhost;Initial Catalog=Financeiro;Integrated Security=True"
+        Dim query As String = "INSERT INTO Transacoes (Nome, CPF, Valor, DataTransacao, Descricao, TipoTransacao) VALUES (@Nome, @CPF, @Valor, @DataTransacao, @Descricao, @TipoTransacao)"
+
+        Using conn As New SqlConnection(connString)
+            Using cmd As New SqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@Nome", TextBox1.Text)
+                cmd.Parameters.AddWithValue("@CPF", TextBox2.Text)
+                cmd.Parameters.AddWithValue("@Valor", Convert.ToDecimal(TextBox3.Text))
+                cmd.Parameters.AddWithValue("@DataTransacao", Convert.ToDateTime(TextBox4.Text)) ' Converte a data corretamente
+                cmd.Parameters.AddWithValue("@Descricao", TextBox6.Text)
+                cmd.Parameters.AddWithValue("@TipoTransacao", TextBox5.Text)
+
+                Try
+                    conn.Open()
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registro adicionado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    CarregarDados() ' Atualiza o DataGridView
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao adicionar o registro: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End Using
+        End Using
+    End Sub
+
+    ' Botão para editar um registro selecionado no DataGridView
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If DataGridView1.SelectedRows.Count = 0 Then
+            MessageBox.Show("Selecione um registro para editar!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim id As Integer = Convert.ToInt32(DataGridView1.SelectedRows(0).Cells("ID").Value)
+        Dim connString As String = "Data Source=localhost;Initial Catalog=Financeiro;Integrated Security=True"
+        Dim query As String = "UPDATE Transacoes SET Nome = @Nome, CPF = @CPF, Valor = @Valor, DataTransacao = @DataTransacao, TipoTransacao = @TipoTransacao WHERE ID = @ID"
+
+        Using conn As New SqlConnection(connString)
+            Using cmd As New SqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@Nome", TextBox1.Text)
+                cmd.Parameters.AddWithValue("@CPF", TextBox2.Text)
+                cmd.Parameters.AddWithValue("@Valor", Convert.ToDecimal(TextBox3.Text))
+                cmd.Parameters.AddWithValue("@DataTransacao", Convert.ToDateTime(TextBox4.Text))
+                cmd.Parameters.AddWithValue("@Descricao", TextBox5.Text)
+                cmd.Parameters.AddWithValue("@TipoTransacao", TextBox5.Text)
+                cmd.Parameters.AddWithValue("@ID", id)
+
+                Try
+                    conn.Open()
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registro editado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    CarregarDados() ' Atualiza o DataGridView
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao editar o registro: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End Using
+        End Using
+    End Sub
+
+    ' Botão para excluir um registro selecionado no DataGridView
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If DataGridView1.SelectedRows.Count = 0 Then
+            MessageBox.Show("Selecione um registro para excluir!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim id As Integer = Convert.ToInt32(DataGridView1.SelectedRows(0).Cells("ID").Value)
+        Dim connString As String = "Data Source=localhost;Initial Catalog=Financeiro;Integrated Security=True"
+        Dim query As String = "DELETE FROM Transacoes WHERE ID = @ID"
+
+        Using conn As New SqlConnection(connString)
+            Using cmd As New SqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@ID", id)
+
+                Try
+                    conn.Open()
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registro excluído com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    CarregarDados() ' Atualiza o DataGridView
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao excluir o registro: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End Using
+        End Using
+    End Sub
+
+End Class
